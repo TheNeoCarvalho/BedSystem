255b80fb3909e48400e61081f3cf73a22aac5d8e
johnnyvaz1 <johnnyjohnnyjohnnyvaz@gmail.com>
2020-03-31 09:33:06 -0300

    inicio

---
+++ docs/Gestao-de-Leitos.mm
@@ -0,0 +1,41 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1585657490078" ID="ID_1419584205" MODIFIED="1585657749270" TEXT="Gest&#xe3;o de Leitos">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1585657520247" ID="ID_1116271097" MODIFIED="1585657528492" POSITION="right" TEXT="Dashboard">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1585657530136" ID="ID_1078562933" MODIFIED="1585657554706" TEXT="gr&#xe1;ficos de uso por cidade">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1585657530136" ID="ID_1869308221" MODIFIED="1585657625655" TEXT="gr&#xe1;ficos de uso por hospital">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1585657636418" ID="ID_1693458922" MODIFIED="1585657672304" TEXT="Gr&#xe1;ficos por tipo">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1585657678700" ID="ID_66791490" MODIFIED="1585657683532" TEXT="UTI">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1585657690543" ID="ID_649808706" MODIFIED="1585657712977" TEXT="Simples(n&#xe3;o UTI)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1585657716165" ID="ID_777552824" MODIFIED="1585657719546" POSITION="left" TEXT="API">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1585657721064" ID="ID_372440130" MODIFIED="1585657736544" TEXT="recebe notifica&#xe7;&#xe3;o por leito">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1585657757791" ID="ID_7634933" MODIFIED="1585657779742" TEXT="recebe status de todos leitos do hospital">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
